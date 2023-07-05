6182bd165f6be6a8ec4883483e241947d8cecf37
engineer_tacky <tsubasa801@gmail.com>
2023-07-05 15:23:17 +0900

    フォームの共通化、タスクの状態変更処理追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,10 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-  {{-- @if (count($tasks)) --}}
+  <div>
+    <a href="{{ route('tasks.create') }}">Add Task!</a>
+  </div>
+
   @forelse ($tasks as $task)
     <div>
       <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
@@ -11,5 +14,10 @@
   @empty
     <div>There are no tasks!</div>
   @endforelse
-  {{-- @endif --}}
+
+  @if ($tasks->count())
+    <nav>
+      {{ $tasks->links() }}
+    </nav>
+  @endif
 @endsection
